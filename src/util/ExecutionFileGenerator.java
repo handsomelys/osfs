3276e6475b52aa317d92ec7580d687181c83693d
lsn42 <24622966+lsn42@users.noreply.github.com>
2020-11-01 21:44:04 +0800

    remove someting

---
+++ src/util/ExecutionFileGenerator.java
@@ -0,0 +1,39 @@
+package util;
+
+import java.util.Random;
+
+public class ExecutionFileGenerator {
+    public static String generateInstructions() {
+        String result = "";
+        Random r = new Random();
+        int count = r.nextInt(10) + 5;
+        for (int i = 0; i < count; ++i) {
+            int choose = r.nextInt(4);
+            String ii = "";
+            if (choose == 0) {
+                ii = "x=" + (r.nextInt(9) + 1);
+            } else if (choose == 1) {
+                ii = "x++";
+            } else if (choose == 2) {
+                ii = "x--";
+            } else if (choose == 3) {
+                ii = "!";
+                choose = r.nextInt(3);
+                if (choose == 0) {
+                    ii = ii + "a";
+                } else if (choose == 1) {
+                    ii = ii + "b";
+                } else if (choose == 2) {
+                    ii = ii + "c";
+                }
+                ii = ii + (r.nextInt(9) + 1);
+            }
+            result = result + ii;
+        }
+        return result + "end";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ExecutionFileGenerator.generateInstructions());
+    }
+}
